094f681361ad9a1a09073e9571670f2f076fc1dd
jt <jt@lanl.gov>
2020-03-02 15:33:19 -0700

    SLCRA had been incorrectly implemented; now fixed.

--- lcra.py
+++ lcra.py
@@ -1,7 +1,6 @@
 ## LCRA: Local Co-Registration Adjustment
 ## For anomalous change detection 
 
-from __future__ import division,print_function,absolute_import
 import numpy as np
 
 def get_drdc(radius,circular=False):
@@ -19,11 +18,10 @@ def get_drdc(radius,circular=False):
                 continue
             drdclist.append((dr,dc))
     ## consider sorting the list by radius dr**2 + dc**2 ... but why bother?
-    #drdclist = sorted( drdclist, key= lambda (r,c): r*r+c*c )
     drdclist = sorted( drdclist, key= lambda rc: rc[0]**2 + rc[1]**2 )
     return drdclist
 
-def lcra_wfcn(imX,imY,acd_fcn,drdclist):
+def lcra_wfcn(imX,imY,acd_fcn,drdclist,reverse=False,symmetric=False):
 
     '''
     LCRA (Local Co-Registration Adjustment) with function acd_fcn
@@ -31,16 +29,26 @@ def lcra_wfcn(imX,imY,acd_fcn,drdclist):
     (eg, acd_fcn = lambda X,Y: acd.apply(X,Y,**kwargs))
     drdclist is list of tuples (dr,dc) defining neighborhood
     ** assumes imX and imY are images, ie 3d arrays **
+
+    reverse==False correspnds to changes X->Y,
+    so that anom[r,c] = min_{dr,dc} acd( X[r+dr,c+dc], Y[r,c] )
+    (since the anomalous change is in Y at Y[r,c])
+    reverse==True corresponds to changes X<-Y, 
+    so that anom[r,c] = min_{dr,dc} acd( X[r,c], Y[r+dr,c+dc] )
+    (since the anomalous change is at X[r,c])
+    symmetric==True corresponds to SLRCR: X<->Y
+    so that anom[r,c] = max( anom_XY[r,c], anom_YX[r,c] )
     '''
     nRows, nCols = imShape = imX.shape[:-1]
     assert( imShape == imY.shape[:-1] )
     dX = imX.shape[-1]
     dY = imY.shape[-1]
 
-    anom_min = np.zeros(imShape)+np.inf
+    anom_min_XY = np.zeros(imShape)+np.inf
+    anom_min_YX = np.zeros(imShape)+np.inf
 
-    assert( len(drdclist) > 0)
     #drdclist = get_drdc(radius=window,circular=circular)
+    assert( len(drdclist) > 0)
 
     for dr,dc in drdclist:
         rsX = slice( max(0,0+dr), min(nRows,nRows+dr) )
@@ -51,17 +59,20 @@ def lcra_wfcn(imX,imY,acd_fcn,drdclist):
         imXtmp = imX[rsX,csX,:]
         imYtmp = imY[rsY,csY,:]
         anom = acd_fcn(imXtmp,imYtmp)
-        #anom_min[rsX,csX] = np.minimum(anom_min[rsX,csX], anom)
-        anom_min[rsY,csY] = np.minimum(anom_min[rsY,csY], anom)
+        anom_min_YX[rsX,csX] = np.minimum(anom_min_YX[rsX,csX], anom)
+        anom_min_XY[rsY,csY] = np.minimum(anom_min_XY[rsY,csY], anom)
 
-    return anom_min     
+    if symmetric:
+        return np.maximum(anom_min_XY,anom_min_YX)
+    elif reverse:
+        return anom_min_YX
+    else:
+        return anom_min_XY
 
 def slcra_wfcn(imX,imY,fcn,drdclist):
     '''
     SLCRA: Symmetric LCRA
     '''
-    anom_XY = lcra_wfcn(imX,imY,fcn,drdclist)
-    anom_YX = lcra_wfcn(imY,imX,fcn,drdclist)
-    anom = np.maximum(anom_XY,anom_YX)
-    return anom
+    return lcra_wfcn(imX,imY,fcn,drdclist,symmetric=True)
+
 
